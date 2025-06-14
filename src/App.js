7cca40423a16fe2f4d667a1ea89fd419bc1d14f0
Githmi99 <108750891+Githmi99@users.noreply.github.com>
2025-06-14 08:32:31 +0530

    Add browserouting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
 import SinhalaResponseSystem from './SinhalaResponseSystem';
 
 function App() {
   return (
+    <BrowserRouter basename="/Signify">
   <div className="App">
       <SinhalaResponseSystem />
     </div>
+    </BrowserRouter>
   );
 }
 
